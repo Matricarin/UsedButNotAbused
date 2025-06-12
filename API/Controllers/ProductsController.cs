9b1a6936d6902c9a2636b45f8ef8ac2f50330dd9
Matricarin <matricarin@gmail.com>
2025-06-12 11:01:49 +0500

    methodsa were became async

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -16,16 +17,16 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public ActionResult<List<Product>> GetProducts()
+        public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = _context.Products.ToList();
+            var products = await _context.Products.ToListAsync();
             return Ok(products);
         }
 
         [HttpGet("{id}")]
-        public string GetProduct(int id)
+        public async Task<Product?> GetProduct(int id)
         {
-            return "single product";
+            return await _context.Products.FindAsync(id);
         }
     }
 }
