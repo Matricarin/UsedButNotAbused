d7763180de65615c7d81b8b7eff4eff25dc92c9d
Matricarin <matricarin@gmail.com>
2025-06-12 09:56:05 +0500

    connection config string was fixed

--- API/Program.cs
+++ API/Program.cs
@@ -3,16 +3,15 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<StoreContext>(c => c.UseSqlite(builder.Configuration.GetConnectionString("DefaultConection")));
+
+builder.Services.AddDbContext<StoreContext>(c =>
+    c.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
