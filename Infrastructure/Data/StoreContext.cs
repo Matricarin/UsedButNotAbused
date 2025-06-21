1d51b4990f2aa1d25ab32c7e8169b2541c5174ac
Matricarin <matricarin@gmail.com>
2025-06-21 15:51:42 +0500

    mogartion was configured

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,6 +16,12 @@ namespace Infrastructure.Data
 
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductBrand> Brands { get; set; }
-        public DbSet<ProductType> ProductTypes { get; set;}
+        public DbSet<ProductType> ProductTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
