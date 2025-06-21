b2facf3ead4fe966bc33048d4b9d753dd455b26c
Matricarin <matricarin@gmail.com>
2025-06-21 16:03:00 +0500

    dbset name was changed

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -15,7 +15,7 @@ namespace Infrastructure.Data
         }
 
         public DbSet<Product> Products { get; set; }
-        public DbSet<ProductBrand> Brands { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
         public DbSet<ProductType> ProductTypes { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
