fde022207f7a65ce6bc5d89791136fd719e00ba6
Matricarin <matricarin@gmail.com>
2025-06-21 16:12:20 +0500

    creating database was added

--- API/Program.cs
+++ API/Program.cs
@@ -20,6 +20,22 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+    try
+    {
+        var context = services.GetRequiredService<StoreContext>();
+        await context.Database.MigrateAsync();
+    }
+    catch (Exception ex)
+    {
+        var logger = loggerFactory.CreateLogger<Program>();
+        logger.LogError(ex, "An error occured during migration");
+    }
+}
+
 app.UseHttpsRedirection();
 
 app.MapControllers();
